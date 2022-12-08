906e5c5991b3abfb03fadc489a2aeab354dfb614
Carlo Lepelaars <info@carlolepelaars.nl>
2022-12-08 22:59:14 +0100

    Augment `setup.py`

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     name='blackscholes',
     version='0.0.1',
     description='',
+    author="Carlo Lepelaars",
+    url="https://github.com/CarloLepelaars/blackscholes",
+    keywords="finance options black scholes merton",
     maintainer='carlolepelaars',
     license='MIT',
     install_requires=[
