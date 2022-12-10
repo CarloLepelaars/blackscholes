Theta with div yield and formula fixes #1

blackscholes/put.py
@@ -46,8 +46,12 @@ class BlackScholesPut(BlackScholesBase):
         """Rate of change in option price
         with respect to time (i.e. time decay).
         """
-        return ((-self.S * norm.pdf(self._d1) * self.sigma) / (2 * np.sqrt(self.T))) + (
+        return (
+            (-np.exp(self.q * self.T) * self.S * norm.pdf(self._d1) * self.sigma)
+            / (2 * np.sqrt(self.T))
+        ) + (
             self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-self._d2)
+            - self.q * self.S * np.exp(-self.q * self.T) * norm.cdf(-self._d1)
         )
 
     def rho(self) -> float: