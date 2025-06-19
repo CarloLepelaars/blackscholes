7a76c0c6f8c5cb01cd550fdf9217c312a9bf1c11
titoconte <tito.conte@gmail.com>
2025-06-19 16:40:44 -0300

    changes vega variable for hide property

--- src/blackscholes/base.py
+++ src/blackscholes/base.py
@@ -102,7 +102,7 @@ class BlackScholesBase(ABC, StandardNormalMixin):
         """
         Rate of change in option price with respect to the volatility of the asset.
         """
-        return self.S * self._pdf(self._d1) * sqrt(self.T)
+        return self._vega
 
     @abstractmethod
     def theta(self) -> float:
@@ -281,6 +281,12 @@ class BlackScholesBase(ABC, StandardNormalMixin):
         """2nd probability parameter that acts as a multiplication factor for discounting."""
         return self._d1 - self.sigma * sqrt(self.T)
 
+    @property
+    def _vega(self) -> float:
+        """
+        Rate of change in option price with respect to the volatility of the asset.
+        """
+        return self.S * self._pdf(self._d1) * sqrt(self.T)
 
 class Black76Base(ABC, StandardNormalMixin):
     """
@@ -334,7 +340,7 @@ class Black76Base(ABC, StandardNormalMixin):
         """Rate of change in option price with respect to the volatility
         of underlying futures contract.
         """
-        return self.F * exp(-self.r * self.T) * self._pdf(self._d1) * sqrt(self.T)
+        return self._vega
 
     @abstractmethod
     def theta(self) -> float:
@@ -406,6 +412,13 @@ class Black76Base(ABC, StandardNormalMixin):
     def _d2(self) -> float:
         """2nd probability parameter that acts as a multiplication factor for discounting."""
         return self._d1 - self.sigma * sqrt(self.T)
+
+    @property
+    def _vega(self) -> float:
+        """Rate of change in option price with respect to the volatility
+        of underlying futures contract.
+        """
+        return self.F * exp(-self.r * self.T) * self._pdf(self._d1) * sqrt(self.T)
 
 
 class BlackScholesStructureBase(ABC):
